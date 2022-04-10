78f22641c0ffca27e3d427066cd405d0a5210258
canewsin <canews.in@gmail.com>
2022-04-10 23:06:43 +0530

    Modify load_peers to add peers to site

--- src/plugins/peer_db.rs
+++ src/plugins/peer_db.rs
@@ -1,8 +1,12 @@
 use rusqlite::params;
+use zeronet_protocol::PeerAddr;
 
-use crate::{controllers::connections::ConnectionController, core::site::Site};
+use crate::{
+    controllers::sites::SitesController,
+    core::{peer::Peer, site::Site},
+};
 
-impl ConnectionController {
+impl SitesController {
     pub fn load_peers(&mut self, site: &Site) {
         let addr = site.address();
         let conn = self.db_manager.get_db("content_db").unwrap();
@@ -11,10 +15,21 @@ impl ConnectionController {
                 a.get::<_, i32>(0)
             })
             .unwrap();
-        let peers = conn.query_row(
-            "SELECT * FROM peer WHERE site_id = :site_id",
-            params![site_id],
-            |r| r.get::<_, String>(1),
-        );
+        let mut stmt = conn
+            .prepare(
+                "SELECT address, port FROM peer WHERE site_id = ? AND address NOT LIKE '%.onion' AND reputation > 0", //TODO! Don't filter out onion peers
+            )
+            .unwrap();
+        let peers = stmt
+            .query_map(params![site_id], |row| {
+                Ok((row.get::<_, String>(0)?, row.get::<_, i32>(1)?))
+            })
+            .unwrap();
+        for peer in peers {
+            let (ip, port) = peer.unwrap();
+            let peer = format!("{}:{}", ip, port);
+            let peer = Peer::new(PeerAddr::parse(&peer).unwrap());
+            self.sites.get_mut(&addr).unwrap().add_peer(peer)
+        }
     }
 }
