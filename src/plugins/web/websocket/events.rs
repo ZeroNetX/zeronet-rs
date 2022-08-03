5366f022a1a75d376c96f68c97e00f1ed5cb22c4
canewsin <canews.in@gmail.com>
2022-08-03 19:10:09 +0530

    websocket::event module changes
    impl Actor for WebsocketController
    modify ServerEvent struct to enum
    Add EventType enum for Events
    Add tests for ServerEvents
    Add RegisterChannels struct

--- src/plugins/web/websocket/events.rs
+++ src/plugins/web/websocket/events.rs
@@ -1,12 +1,18 @@
-use std::time::Duration;
+use std::collections::HashMap;
 
 use actix::prelude::*;
+use serde::{Deserialize, Serialize};
 
-use super::*;
-use crate::core::error::Error;
+use crate::core::{error::Error, site::models::SiteInfo};
+
+use super::{handlers::tracker::AnnouncerStats, ServerInfo, ZeruWebsocket};
 
 pub struct WebsocketController {
     pub listeners: Vec<Addr<ZeruWebsocket>>,
+}
+
+impl Actor for WebsocketController {
+    type Context = Context<Self>;
 }
 
 #[derive(Message)]
@@ -19,35 +25,87 @@ impl Handler<RegisterWSClient> for WebsocketController {
     type Result = Result<(), Error>;
 
     fn handle(&mut self, msg: RegisterWSClient, _ctx: &mut Context<Self>) -> Self::Result {
-        self.listeners.push(msg.addr);
-        Ok(())
+        Ok(self.listeners.push(msg.addr))
     }
 }
 
-#[derive(Message)]
+#[derive(Serialize, Deserialize, Message)]
 #[rtype(result = "()")]
-struct ServerEvent {
-    event: String,
+#[serde(rename_all = "camelCase")]
+pub enum ServerEvent {
+    Event {
+        cmd: String,
+        params: EventType,
+    },
+    #[serde(rename = "event")]
+    EventString(String),
 }
 
-impl Actor for WebsocketController {
-    type Context = Context<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        ctx.run_interval(Duration::from_secs(5), |act, _| {
-            for listener in &act.listeners {
-                listener.do_send(ServerEvent {
-                    event: "{\"cmd\":\"setAnnouncerInfo\", \"params\":{\"address\":null,\"stats\":{\"custom_tracker\":{\"status\":\"announced\"}}}}".to_string(),
-                });
-            }
-        });
-    }
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+#[serde(rename_all = "camelCase")]
+pub enum EventType {
+    AnnouncerInfo {
+        address: String,
+        stats: HashMap<String, AnnouncerStats>,
+    },
+    ServerInfo(ServerInfo),
+    SiteInfo(SiteInfo),
 }
 
 impl Handler<ServerEvent> for ZeruWebsocket {
     type Result = ();
 
     fn handle(&mut self, msg: ServerEvent, ctx: &mut Self::Context) {
-        ctx.text(msg.event);
+        ctx.text(serde_json::to_string(&msg).unwrap());
+    }
+}
+
+#[derive(Message)]
+#[rtype(result = "Result<(), Error>")]
+pub struct RegisterChannels(pub Vec<String>);
+
+impl Handler<RegisterChannels> for ZeruWebsocket {
+    type Result = Result<(), Error>;
+
+    fn handle(&mut self, msg: RegisterChannels, _ctx: &mut Self::Context) -> Self::Result {
+        let channels = msg.0;
+        println!("RegisteringChannels: {:?}", channels);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::{super::handlers::tracker::AnnouncerStats, EventType, ServerEvent};
+
+    fn def_ann_info() -> ServerEvent {
+        let mut map = HashMap::new();
+        map.insert(
+            String::from("zero://boot3rdez4rzn36x.onion:15441"),
+            AnnouncerStats {
+                status: "announced".into(),
+                num_request: 20,
+                num_success: 15,
+                num_error: 5,
+                ..Default::default()
+            },
+        );
+        ServerEvent::Event {
+            cmd: "setAnnouncerInfo".into(),
+            params: EventType::AnnouncerInfo {
+                address: "1HELLoE3sFD9569CLCbHEAVqvqV7U2Ri9d".into(),
+                stats: map,
+            },
+        }
+    }
+
+    #[test]
+    fn test_announcer_info() {
+        let info = def_ann_info();
+        let json = serde_json::to_string_pretty(&info).unwrap();
+        println!("{:#}", json);
     }
 }
