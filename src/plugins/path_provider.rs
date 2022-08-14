abe13dda5e08ec70ed8afd25fc8115ffa7d96152
canewsin <canews.in@gmail.com>
2022-08-15 04:02:34 +0530

    Add impl_plugin macro to auto gen load_plugins fn Removed manual load_path_provider_plugins fn infavor of impl_plugin!

--- src/plugins/path_provider.rs
+++ src/plugins/path_provider.rs
@@ -1,16 +1,21 @@
 use std::ops::DerefMut;
 
-use mut_static::MutStatic;
-use wit_bindgen_wasmer::wasmer::{imports, Cranelift, Module, Store};
-
-use crate::environment::{ENV, PATH_PROVIDER_PLUGINS, PLUGINS};
+use log::error;
 
 use self::path_provider::PathProvider;
 
-use super::Permission;
-use log::error;
+use crate::{
+    environment::{ENV, PATH_PROVIDER_PLUGINS},
+    impl_plugin,
+    plugins::Permission,
+};
 
-wit_bindgen_wasmer::import!("assets/plugins/path_provider.wit");
+impl_plugin!(
+    PathProviderPlugin,
+    PathProvider,
+    "assets/plugins/path_provider.wit",
+    Permission::PathProvider
+);
 
 pub fn get_storage_path() -> String {
     let mut plugin = (*PATH_PROVIDER_PLUGINS).write().unwrap();
@@ -36,41 +41,4 @@ pub fn get_file_path(block_id: &str) -> String {
         error!("Failed to get storage path");
     }
     path.unwrap()
-}
-
-pub fn load_path_provider_plugins() -> MutStatic<Vec<PathProviderPlugin>> {
-    // let plugins_dir = PathBuf::from("plugins");
-    let plugins = (&*PLUGINS)
-        .iter()
-        .filter(|p| p.permissions.contains(&Permission::PathProvider));
-    let mut plugins_loaded = Vec::new();
-    for plugin in plugins {
-        let engine = Cranelift::default();
-        let mut store = Store::new(engine);
-        let bytes = std::fs::read(&plugin.path).unwrap();
-        let module = Module::new(&store, &bytes);
-        if let Ok(module) = module {
-            let mut imports = imports! {};
-            let funs = PathProvider::instantiate(&mut store, &module, &mut imports);
-            if let Ok((provider, _)) = funs {
-                let plugin = PathProviderPlugin {
-                    store,
-                    module,
-                    provider,
-                };
-                plugins_loaded.push(plugin);
-            } else {
-                let error = funs.err().unwrap();
-                error!("Failed to load plugin {:?}", error);
-            }
-        }
-    }
-    MutStatic::from(plugins_loaded)
-}
-
-#[allow(unused)]
-pub struct PathProviderPlugin {
-    store: Store,
-    module: Module,
-    provider: PathProvider,
 }
