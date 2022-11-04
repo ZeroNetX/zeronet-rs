1226848163851b2da6ff34e8fdb01906a8121bb6
canewsin <canews.in@gmail.com>
2022-11-04 22:18:56 +0530

    site_server::handlers -> Add get_wrapper_key & get_ajax_key fns for Site

--- src/plugins/site_server/handlers/mod.rs
+++ src/plugins/site_server/handlers/mod.rs
@@ -1,2 +1,26 @@
+use crate::{core::site::Site, plugins::site_server::common::get_nonce};
+
 pub mod sites;
 pub mod users;
+
+impl Site {
+    pub fn get_wrapper_key(&mut self) -> String {
+        if self.storage.keys.wrapper_key.is_empty() {
+            let key = get_nonce(false, 64);
+            self.storage.keys.wrapper_key = key.clone();
+            key
+        } else {
+            self.storage.keys.wrapper_key.clone()
+        }
+    }
+
+    pub fn get_ajax_key(&mut self) -> String {
+        if self.storage.keys.ajax_key.is_empty() {
+            let key = get_nonce(false, 64);
+            self.storage.keys.ajax_key = key.clone();
+            key
+        } else {
+            self.storage.keys.ajax_key.clone()
+        }
+    }
+}
